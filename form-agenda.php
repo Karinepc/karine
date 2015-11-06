c0b4124c580a7141427b771a713916cbee9c6b0b
Karinepc <karinepires01@hotmail.com>
2015-11-06 20:07:41 -0200

    Create form-agenda.php

---
+++ form-agenda.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Adicionar Contato</title>
+</head>
+<body>
+    <form action="verif-agenda.php" method="post">
+        Nome: <input type="text" name="nome_cont"><br/>
+		Numero:<input type="text" name="numero_cont"><br/>
+		<input type="submit" value="enviar"/> 
+
+</body>
+</html>
